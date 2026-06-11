057be30843669b8e872334d4ef93dc5a7a855dd6
Oleg Skvortsov <highsoft.soi@gmail.com>
2026-06-10 20:43:32 -0700

    Added comment for checking raw signal from transformer

--- SCT-013_transformer.ino
+++ SCT-013_transformer.ino
@@ -1,3 +1,33 @@
+/*
+First step — check raw signal
+
+Upload this code:
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int v = analogRead(A0);
+  Serial.println(v);
+  delay(10);
+}
+
+What you should see
+
+With NO load:
+
+Values fluctuate around ~512 (middle point)
+
+With load ON (kettle, heater, etc.):
+
+Wave-like fluctuation around 512, e.g.:
+480 → 550 → 500 → 530 → ...
+
+If this works → hardware is correct.
+*/
+
+
 
 void setup() {
 
