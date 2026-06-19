d13893b8380272a0dd91a4e552431a23ce3053a0
Oleg Skvortsov <highsoft.soi@gmail.com>
2026-06-18 21:59:28 -0700

    Added functionality for connecting to Wifi and publish current into HA

--- SCT-013_transformer.ino
+++ SCT-013_transformer.ino
@@ -27,14 +27,66 @@ void loop() {
 
 //If this works → hardware is correct.
 
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
+#define wifi_ssid "Soi13"
+#define wifi_password ""
+
+#define mqtt_server "192.168.1.64"
+#define mqtt_user "mqtt_user"
+#define mqtt_password ""
+
+#define dryer_current "homeassistant/sensor/dryer_current"
+#define dryer_on_off "homeassistant/sensor/dryer_on_off"
 
 #define SAMPLES 1000
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 const float ADC_REF = 3.3;        // NodeMCU ADC reference
 const float ADC_MAX = 1024.0;     // 10-bit ADC
 const float BURDEN = 30.0;        // 30Ω (3x10Ω)
 const float CT_RATIO = 2000.0;    // SCT-013-000 = 100A : 50mA => 2000:1
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connectMQTT() {
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT...");
+
+    String clientId = "ESP8266-";
+    clientId += String(ESP.getChipId());
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retry in 5 sec");
+      delay(5000);
+    }
+  }
+}
 
 float calcIrms() {
   float sumSq = 0;
@@ -61,14 +113,40 @@ float calcIrms() {
 
 void setup() {
   Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  connectMQTT();
 }
 
 void loop() {
+  // Check WiFi
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi lost. Reconnecting...");
+    setup_wifi();
+  }
+
+  // Check MQTT
+  if (!client.connected()) {
+    Serial.println("MQTT disconnected. Reconnecting...");
+    connectMQTT();
+  }
+
+  client.loop();
+
   float Irms = calcIrms();
 
-  Serial.print("Current: ");
-  Serial.print(Irms);
-  Serial.println(" A");
+  //If current more than 5A we count that dryer is working and show status as On and display current, otherwise show status is Off and current = 0
+  if (Irms > 5.0) {
+    client.publish(dryer_on_off, "Dryer is On");
+    client.publish(dryer_current, String(Irms).c_str());
+  } else {
+    client.publish(dryer_on_off, "Dryer is Off");
+    client.publish(dryer_current, "0");
+  }
+
+  //Serial.print("Current: ");
+  //Serial.print(Irms);
+  //Serial.println(" A");
 
   delay(1000);
 }
